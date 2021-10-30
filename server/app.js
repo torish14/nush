9b71dd08cd09ffd06e058cde61b5f57e9c402f5b
toshihirokato <nanamin.104@icloud.com>
2021-10-30 23:13:41 +0900

    usersRouter の記述を削除

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ var logger = require('morgan')
 const cors = require('cors')
 
 var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
 
 var app = express()
 
@@ -20,6 +19,5 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 module.exports = app
